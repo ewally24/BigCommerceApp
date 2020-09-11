06d1259ad192f2c2d26b4751b6a6979435f38323
Errol T. Wallace <ewally24@users.noreply.github.com>
2020-09-10 21:35:11 -0400

    Add Index form template Setup Bootstrap CSS for Client Form

--- app.js
+++ app.js
@@ -21,12 +21,17 @@ var uninstall = require("./routes/uninstall");
 // ========================================================
 
 var app = express();
+
+// view engine setup
+app.set("views", __dirname + "/views/partials");
+app.set("view engine", "hbs");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.set("views", __dirname + "/views");
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
@@ -36,6 +41,6 @@ app.use("/load", load);
 app.use("/uninstall", uninstall);
 // ========================================================
 
-var listener = app.listen(8080, function() {
+var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
 });
